26c5f568517614f381b85f22770b25e7d0f1ff5b
Рашид <valentaine1234@gmail.com>
2025-07-16 15:25:20 +0300

    rivew fix

--- tests/scenarios/test_cards_form.py
+++ tests/scenarios/test_cards_form.py
@@ -2,8 +2,8 @@ import allure
 
 from tests.test_base import TestBase
 from userinyerface.forms.email_password_card import EmailPasswordForm
-# from userinyerface.forms.interests_avatar_card import InterestsAvatarForm
-# from userinyerface.forms.more_info_card import MoreInfoForm
+from userinyerface.forms.interests_avatar_card import InterestsAvatarForm
+from userinyerface.forms.more_info_card import MoreInfoForm
 from userinyerface.utils.faker_factory import FakeUserFactory
 
 
@@ -11,7 +11,7 @@ class TestCardsForm(TestBase):
 
     def test_card_form(self):
         with allure.step("Start registration and load data"):
-            # test_data = self.prepare_test_data()
+            test_data = self.prepare_test_data()
 
             self.open_main_form_from_start()
             email_form = EmailPasswordForm()
@@ -25,19 +25,19 @@ class TestCardsForm(TestBase):
             email_form.select_domain_zone(user_data.domain_zone)
             email_form.terms_checkbox_chek()
 
-        # with allure.step("Navigate to the Interests and Avatar card"):
-        #     email_form.next_button_click()
-        #     interests_avatar_form = InterestsAvatarForm()
-        #     assert interests_avatar_form.state.wait_for_displayed(), "Interest and avatar card is not open"
+        with allure.step("Navigate to the Interests and Avatar card"):
+            email_form.next_button_click()
+            interests_avatar_form = InterestsAvatarForm()
+            assert interests_avatar_form.state.wait_for_displayed(), "Interest and avatar card is not open"
 
-        # with allure.step("Select interests and upload avatar"):
-        #     interests_avatar_form.uncheck_unselect_all()
-        #     interests_avatar_form.select_two_random_interests(test_data.interest_selection_count)
-        #
-        #     interests_avatar_form.upload_avatar_image(test_data.avatar_name)
-        #     assert interests_avatar_form.avatar_is_uploaded(), "Avatar is not uploaded"
-        #
-        # with allure.step("Go to more info card"):
-        #     email_form.next_button_click()
-        #     more_info_form = MoreInfoForm()
-        #     assert more_info_form.state.wait_for_displayed(), "More info card is not open"
+        with allure.step("Select interests and upload avatar"):
+            interests_avatar_form.uncheck_unselect_all()
+            interests_avatar_form.select_two_random_interests(test_data.interest_selection_count)
+
+            interests_avatar_form.upload_avatar_image(test_data.avatar_name)
+            assert interests_avatar_form.avatar_is_uploaded(), "Avatar is not uploaded"
+
+        with allure.step("Go to more info card"):
+            email_form.next_button_click()
+            more_info_form = MoreInfoForm()
+            assert more_info_form.state.wait_for_displayed(), "More info card is not open"
--- tests/units/test_timer_start.py
+++ tests/units/test_timer_start.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import allure
 
 from tests.test_base import TestBase
@@ -5,6 +7,7 @@ from tests.test_base import TestBase
 
 class TestHelpForm(TestBase):
 
+    @pytest.mark.parametrize("expected_start", [timedelta(0)])
     def test_timer_start(self):
         with allure.step("Prepare test data and open main form"):
             main_form = self.open_main_form_from_start()
@@ -12,7 +15,7 @@ class TestHelpForm(TestBase):
 
         with allure.step("Verify timer starts from expected time"):
             actual_timer = main_form.get_timer_started_from()
-            expected_timer = test_data.timer_start
+            expected_timer = timedelta(0)
 
             assert actual_timer == expected_timer, (
                 f"Expected timer {expected_timer} != {actual_timer}"
